50b91f47c97caa5eb761cb4671122c88f3355eb2
Colin Silcock <colsil@users.noreply.github.com>
2017-06-06 08:49:42 +0100

    Add first version of show_facts method

--- napalm_asa/asa.py
+++ napalm_asa/asa.py
@@ -1,5 +1,3 @@
-# Copyright 2016 Dravetech AB. All rights reserved.
-#
 # The contents of this file are licensed under the Apache License, Version 2.0
 # (the "License"); you may not use this file except in compliance with the
 # License. You may obtain a copy of the License at
@@ -17,9 +15,13 @@ Napalm driver for Cisco ASA.
 
 Read https://napalm.readthedocs.io for more information.
 """
+from __future__ import print_function
+from __future__ import unicode_literals
 from difflib import unified_diff
-
+import re
 from napalm_base.base import NetworkDriver
+from napalm_base.utils import py23_compat
+from napalm_ios import IOSDriver
 
 from netmiko import ConnectHandler
 
@@ -78,6 +80,7 @@ class AsaDriver(NetworkDriver):
         """Wrapper for self.device.send.command().
         If command is a list will iterate through commands until valid command.
         """
+        output = ""
         if isinstance(command, list):
             for cmd in command:
                 output = self.device.send_command(cmd)
@@ -133,3 +136,64 @@ class AsaDriver(NetworkDriver):
 
     def commit_config(self):
         raise NotImplementedError
+
+    def get_lldp_neighbors(self):
+        '''ASA implementation of get_lldp_neighbors
+        Always returns empty dict as ASA does not support CDP or LLDP
+        '''
+        return {}
+
+    def get_lldp_neighbors_detail(self, interface=""):
+        """"ASA Implementation of get_lldp_neighbors_detail
+        Always returns empty dict as ASA does not support CDP or LLDP
+        """
+        return {}
+
+    def get_facts(self):
+        vendor = u'Cisco'
+        uptime = -1
+        serial_number, fqdn, os_version, hostname, model = (u'Unknown', u'Unknown', u'Unknown', u'Unknown', u'Unknown')
+
+        show_ver = self._send_command('show version')
+        show_hostname = self._send_command('show hostname')
+        show_fqdn = self._send_command('show hostname fqdn')
+        show_inventory = self._send_command('show inventory')
+
+        # Hostname and FQDN are returned by show commands without any other output
+        hostname = show_hostname.strip()
+        fqdn = show_fqdn.strip()
+
+        # Parse show version command
+        # Gather os version, uptime and serial number
+        for line in show_ver.splitlines():
+            if 'Cisco Adaptive Security Appliance Software Version' in line:
+                _, os_version = re.split(r"(Cisco Adaptive Security Appliance Software Version )(\S*)")
+                os_version = os_version.strip()
+            if hostname + ' up ' in line:
+                _, uptime_str = line.split(' up ')
+                uptime = IOSDriver.parse_uptime(uptime_str)
+            if "Serial Number: " in line:
+                _, serial_number = line.split(' Number: ')
+                serial_number = serial_number.strip()
+
+        chassis_flag = False
+        for line in show_inventory.splitlines():
+            if "Name: \"Chassis\"" in line:
+                chassis_flag = True
+            if chassis_flag and "PID: " in line:
+                match = re.search(r'PID: (\S*) .*',line)
+                model = match.group(0)
+
+        # Build interface list
+        interface_list = []
+
+        return {
+            'uptime': uptime,
+            'vendor': vendor,
+            'os_version': py23_compat.text_type(os_version),
+            'serial_number': py23_compat.text_type(serial_number),
+            'model': py23_compat.text_type(model),
+            'hostname': py23_compat.text_type(hostname),
+            'fqdn': fqdn,
+            'interface_list': interface_list
+        }
